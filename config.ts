e09829a0ebfcde591a098880164c24dd122c45f2
kkldream <kk69347321@gmail.com>
2022-12-09 15:02:59 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,14 +1,17 @@
 export default {
-  foodTypeList: [
-    "cafe",
-    "food",
-    "restaurant",
-    "meal_takeaway"
-  ],
-  minResponseCount: 1,
-  drawCard: {
-    maxDistance: 1000,
-    ratingStar: 4,
-    ratingTotal: 100
-  }
+    foodTypeList: [
+        "cafe",
+        "food",
+        "restaurant",
+        "meal_takeaway"
+    ],
+    minResponseCount: 1,
+    drawCard: {
+        maxDistance: 1000,
+        ratingStar: 4,
+        ratingTotal: 100
+    },
+    blackList: [
+        "ChIJhzvq_PquQjQR8ScFcGZtnrY"
+    ]
 }
