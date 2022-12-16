23b2c520458d109368c307c7d1230492bf09063a
kkldream <kk69347321@gmail.com>
2022-12-16 17:26:52 +0800

    Update mongodbMgr.ts

--- models/mongodbMgr.ts
+++ models/mongodbMgr.ts
@@ -17,8 +17,8 @@ export default class MongodbClient {
         // Db List
         let foodMapDb = client.db('food_map');
         // Collection List
-        this.foodMapDb.placeCol = foodMapDb.collection('googlePlace');
-        this.foodMapDb.userCol = foodMapDb.collection('userV2');
+        this.foodMapDb.placeCol = foodMapDb.collection('place');
+        this.foodMapDb.userCol = foodMapDb.collection('user');
         this.foodMapDb.loginLogCol = foodMapDb.collection('userLog');
         this.foodMapDb.updateLogCol = foodMapDb.collection('searchLog');
         this.foodMapDb.photoCol = foodMapDb.collection('photo');
