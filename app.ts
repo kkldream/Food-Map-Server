e67f26edf90bfa639c04c7c863b4f02262822999
kkldream <kk69347321@gmail.com>
2022-12-31 13:33:25 +0800

    opt express.json()

--- app.ts
+++ app.ts
@@ -12,7 +12,7 @@ dotenv.config();
 // init express
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.json());
+
 app.use(require('cors')());
 app.use(session({
     secret: 'mySecret',
--- routes/api.ts
+++ routes/api.ts
@@ -6,16 +6,60 @@ import googleApiRoute from './api/googleApiRoute';
 import userRoute from './api/userRoute';
 import rootRoute from "./api/rootRoute";
 import {routeApiLogDocument} from "../models/dataStruct/mongodb/routeApiLogDocument";
+import {baseResponses} from "../models/dataStruct/response/baseResponse";
 
 const router = Router()
 
-router.use('/', function (req: any, res: any, next: any) {
+router.use(function (req: any, res: any, next: any) {
+    req.requestTime = new Date();
+    next();
+});
+
+// 實作express.json()方法，改善在json body裡放註解不會抱錯
+router.use(async function (req: any, res: any, next: any) {
+    let buffers = [];
+    for await (let chunk of req) buffers.push(chunk);
+    let dataStr = Buffer.concat(buffers).toString();
+    // 字串處理
+    let output = "";
+    let slashToken = false;
+    let doubleQuotesToken = false;
+    let isAnnotationStatus = false;
+    for (let index = 0; index < dataStr.length; index++) {
+        let data = dataStr[index];
+        if (data === "/") slashToken = !slashToken;
+        if (data === "\"") doubleQuotesToken = !doubleQuotesToken;
+        if (!doubleQuotesToken && data === " ") continue;
+        if (!doubleQuotesToken && data === "/") {
+            isAnnotationStatus = true;
+            continue;
+        }
+        if (isAnnotationStatus && (data === "\r" || data === "\n")) {
+            isAnnotationStatus = false;
+            continue;
+        }
+        if (!isAnnotationStatus) output += data;
+    }
+    // json轉換
+    try {
+        req.body = JSON.parse(output);
+        next();
+    } catch (err) {
+        res.send(({
+            requestTime: req.requestTime,
+            status: -1,
+            errMsg: "JSON解析錯誤"
+        } as baseResponses));
+    }
+});
+
+router.use(function (req: any, res: any, next: any) {
     if (req.session.userId) req.body.userId = req.session.userId;
     if (req.session.accessKey) req.body.accessKey = req.session.accessKey;
     next();
 });
 
-router.use('/', function (req: any, res: any, next: any) {
+router.use(function (req: any, res: any, next: any) {
     const routeApiLogCol = global.mongodbClient.foodMapDb.routeApiLogCol;
     let routeApiLogDoc: routeApiLogDocument = {
         createTime: new Date(),
